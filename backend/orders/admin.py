08abcaf791d6d81716f697c68aad2aef391b69d2
Vivek Ramesh More <morevivek857@gmail.com>
2025-11-23 08:20:23 -0800

    index.html

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import DeliverySlot,Order,OrderItem
 # Register your models here.
+admin.site.register(DeliverySlot)
+admin.site.register(Order)
+admin.site.register(OrderItem)
