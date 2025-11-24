3b3fcc8a3a6e59215f4e8e22d6db6742b83fd3ed
Vivek Ramesh More <morevivek857@gmail.com>
2025-11-23 21:15:25 -0800

    category

--- backend/products/models.py
+++ backend/products/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100,unique=True)
+    description = models.TextField(blank=True,null=True)
+    image = models.ImageField(upload_to='category/')
 
     def __str__(self):
         return self.name
